7c9f62cbbf532e35a006f69705d4cbf60d674c4c
brianm16 <amirbaam9@gmail.com>
2025-05-30 21:44:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add JavaScript here for interactive elements later
+// For example, to add a smooth scrolling effect for the navigation links:
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
